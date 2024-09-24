e6058b86b54aaed5d9c7f436724927abff8b2b3c
vini1702 <142527411+vini1702@users.noreply.github.com>
2024-09-24 12:00:49 -0300

    a

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calcule a área do quadrado</title>
+    <title>Cálculo da Área do Quadrado</title>
 </head>
 <body>
-    <h2>Calcular a área do quadrado</h2>
-    <label for="lado">Informe o valor do lado do quadrado:</label>
-    <input type="number" id="lado" min="0" step="0.01"
-    placeholder="Digite o valor do lado">
-        <button
-    onclick="calcularArea()">Calcular</button>    
-   
-<p id="resultado"></p>
+    <h2>Calcular a Área de um Quadrado</h2>
 
-<script>
-    function calcularArea() {
-        const lado = document.getElementById("lado").value;
-        if (isNaN*lado) || lado <= 0)}
-        document.getElementById("resultado") .{
-        innerText + "Por favor, insira um número válido maior que zero";
-               return;
+    <label for="lado">Informe o valor do lado do quadrado:</label>
+    <input type="number" id="lado" min="0" step="0.01" placeholder="Digite o valor do lado">
+    <button onclick="calcularArea()">Calcular</button>
+
+    <p id="resultado"></p>
+
+    <script>
+        function calcularArea() {
+           
+            const lado = parseFloat(document.getElementById("lado").value);
+
+            
+            if (isNaN(lado) || lado <= 0) {
+                document.getElementById("resultado").innerText = "Por favor, insira um número válido maior que zero.";
+                return;
             }
 
+            
             const area = lado * lado;
 
-    document.getElementById("resultado") . 
-    innerText = 'A área do quadrado é: ${area}';
+            
+            document.getElementById("resultado").innerText = A área do quadrado é: ${area};
         }
-    
-
-</script>
+    </script>
 </body>
 </html>
