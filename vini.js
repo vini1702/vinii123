e5067e8de71b19c290cde213e8e791e22d2b409c
vini1702 <142527411+vini1702@users.noreply.github.com>
2024-09-24 12:07:40 -0300

    a

--- vini.js
+++ vini.js
@@ -1,18 +1,21 @@
-function calcularAreaQuadrado() {
-    const lado = parseFloat(prompt("Digite o valor do lado do quadrado:"));
+function calcularMedia() {
+
+    const valor1 = parseFloat(prompt("Digite o primeiro valor:"));
+    const valor2 = parseFloat(prompt("Digite o segundo valor:"));
+    const valor3 = parseFloat(prompt("Digite o terceiro valor:"));
+
     
-    
-    if (isNaN(lado) || lado <= 0) {
-        console.log("Por favor, insira um número válido maior que zero.");
+    if (isNaN(valor1) || isNaN(valor2) || isNaN(valor3)) {
+        console.log("Por favor, insira apenas números válidos.");
         return;
     }
 
-    
-    const area = lado * lado;
-    
-    
-    console.log(A área do quadrado é: ${area.toFixed(2)});
+   
+    const media = (valor1 + valor2 + valor3) / 3;
+
+ 
+    console.log(A média dos três valores é: ${media.toFixed(2)});
 }
 
 
-calcularAreaQuadrado();
+calcularMedia();
