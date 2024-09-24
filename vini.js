f448cfb5f7bec09197459275f1b0d0dab338c77a
vini1702 <142527411+vini1702@users.noreply.github.com>
2024-09-24 12:26:41 -0300

    a

--- vini.js
+++ vini.js
@@ -1,13 +1,17 @@
 function calcularMedia() {
+=
+    let valor1 = +prompt("Digite o primeiro valor:");
+    let valor2 = +prompt("Digite o segundo valor:");
+    let valor3 = +prompt("Digite o terceiro valor:");
 
-    const valor1 = +prompt("Digite o primeiro valor:");
-    const valor2 = +prompt("Digite o segundo valor:");
-    const valor3 = +prompt("Digite o terceiro valor:");
+ 
+    let media = (valor1 + valor2 + valor3) / 3;
 
-
-    return (valor1 + valor2 + valor3) / 3;
+ 
+    return media;
 }
 
 
-const media = calcularMedia();
-console.log(A média dos três valores é: ${media});
+let resultado = calcularMedia();
+
+console.log(A média dos três valores é: ${resultado});
