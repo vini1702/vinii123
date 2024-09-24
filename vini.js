9fbca416e7698f94ac698d7d12075221485e1a45
vini1702 <142527411+vini1702@users.noreply.github.com>
2024-09-24 11:42:28 -0300

    a

--- vini.js
+++ vini.js
@@ -1,4 +1,9 @@
 function soma(num1,num2){
 return num1 + num2;
 }
+console.log(soma(2,3));
 
+function calcularAreaQuadrado(lado) {
+   return lado * lado:
+}
+console.log("Área do quadrado é:" + calcularAreaQuadrado)
